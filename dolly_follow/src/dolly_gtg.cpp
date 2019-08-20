bf8abca186271c15d3d8efb02639d10a3e7970bc
Emiliano Borghi <eborghi@ekumenlabs.com>
2019-08-20 20:45:15 -0300

    Fix GtG behavior

--- dolly_follow/src/dolly_gtg.cpp
+++ dolly_follow/src/dolly_gtg.cpp
@@ -19,9 +19,10 @@
 #include <utility>
 
 #include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/point.hpp>
 #include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 
 #include <tf2/utils.h>
@@ -46,6 +47,7 @@ public:
         {
             this->goal_pose_ = _msg->pose;
             this->initial_goal_ = true;
+            this->print_once_ = true;
         },
         rmw_qos_profile_sensor_data,
         cb_grp1_
@@ -66,48 +68,134 @@ public:
     // Advertise velocity commands
     cmd_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel");
 
-    timer_ = this->create_wall_timer(500ms, std::bind(&GoToGoal::OnTimer, this), cb_grp3_);
+    timer_ = this->create_wall_timer(100ms, std::bind(&GoToGoal::OnTimer, this), cb_grp3_);
   }
 
 private:
   void OnTimer()
   {
-    // Skip until we have first messages
-    if (!initial_goal_ || !initial_gt_)
-        return;
+    // https://github.com/merose/diff_drive/blob/master/src/diff_drive/goal_controller.py
     
+    // Skip until we have first messages
+    if (!initial_goal_ || !initial_gt_) return;
+
     // Populate command message, all weights have been calculated by trial and error
     auto cmd_msg = std::make_shared<geometry_msgs::msg::Twist>();
     
+    if (atGoal(gt_msg_, goal_pose_)) {
+      // Stopping robot -- sending empty velocity message
+      cmd_pub_->publish(cmd_msg);
+      if (print_once_)
+      {
+        print_once_ = false;
+        RCLCPP_INFO(this->get_logger(), "Goal reached");
+        std::flush(std::cout);
+      }
+      return;
+    }
+    
     const double dX(goal_pose_.position.x - gt_msg_.position.x);
     const double dY(goal_pose_.position.y - gt_msg_.position.y);
+    const double goal_heading = std::atan2(dY, dX);
+    double a = goal_heading - yawFromQuaternion(gt_msg_.orientation);
 
-    const double lin_vel = std::sqrt(dX * dX + dY * dY);
-    cmd_msg->linear.x = lin_vel > max_lin_vel_? max_lin_vel_ : lin_vel;
+    const double curr_theta = yawFromQuaternion(gt_msg_.orientation);
+    const double goal_theta = yawFromQuaternion(goal_pose_.orientation); 
+    const double theta = normalizePi(curr_theta - goal_theta);
+    double b = -theta -a;
 
-    tf2::Quaternion q(
-      gt_msg_.orientation.x,
-      gt_msg_.orientation.y,
-      gt_msg_.orientation.z,
-      gt_msg_.orientation.w
-    );
-    tf2::Matrix3x3 m(q);
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
+    const double d = getGoalDistance(gt_msg_.position, goal_pose_.position);
+    const double direction = 1;
+    a = normalizePi(a);
+    b = normalizePi(b);
 
-    double angle = std::atan2(dY, dX) - yaw;
+    if (std::abs(d) < lin_tol_)
+    {
+      cmd_msg->linear.x  = 0;
+      cmd_msg->angular.z = kB * theta;
+    }
+    else
+    {
+      cmd_msg->linear.x  = kP * d * direction;
+      cmd_msg->angular.z = kA * a + kB * b;
+    }
 
-    angle = std::fmod(angle + M_PI, TWO_M_PI_);
-    if (angle < 0)
-        angle += TWO_M_PI_;
-    angle -= M_PI;
-
-    cmd_msg->angular.z = std::abs(angle) < angle_tol_ ? 0 : angle;
-
-    RCLCPP_INFO(this->get_logger(), "Goal: [%s, %f]", std::to_string(goal_pose_.position.x).c_str(), goal_pose_.position.y);
-    std::flush(std::cout);
+    adjustVelocities(cmd_msg);
 
     cmd_pub_->publish(cmd_msg);
+  }
+
+  double getGoalDistance(
+      const geometry_msgs::msg::Point &curr, 
+      const geometry_msgs::msg::Point &goal)
+  {
+    const double dX(curr.x - goal.x);
+    const double dY(curr.y - goal.y);
+
+    return std::sqrt(dX * dX + dY * dY);
+  }
+
+  bool atGoal(
+      const geometry_msgs::msg::Pose &curr, 
+      const geometry_msgs::msg::Pose &goal)
+  {
+    // Linear distance from goal
+    const double dist = getGoalDistance(curr.position, goal.position);
+    // Angular distance from goal
+    const double curr_theta = yawFromQuaternion(curr.orientation);
+    const double goal_theta = yawFromQuaternion(goal.orientation);
+    const double dAngle = std::abs(normalizePi(curr_theta - goal_theta));
+    
+    return dist < lin_tol_ && dAngle < angle_tol_;
+  }
+
+  double normalizePi(const double &angle)
+  {
+    double normAngle(angle);
+    while (normAngle >  M_PI){
+      normAngle -= TWO_M_PI_;
+    }
+    while (normAngle < -M_PI){
+      normAngle += TWO_M_PI_;
+    }
+    return normAngle;
+  }
+
+  double yawFromQuaternion(const geometry_msgs::msg::Quaternion &q)
+  {
+    tf2::Quaternion q_tf(q.x, q.y, q.z, q.w);
+    tf2::Matrix3x3 m(q_tf);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    return yaw;
+  }
+
+  void adjustVelocities(geometry_msgs::msg::Twist::SharedPtr &cmd)
+  {
+    if (cmd->linear.x > max_lin_vel_)
+    {
+      const double ratio = max_lin_vel_ / std::abs(cmd->linear.x);
+      cmd->linear.x *= ratio;
+      cmd->angular.z *= ratio;
+    }
+    if (cmd->angular.z > max_ang_vel_)
+    {
+      const double ratio = max_ang_vel_ / std::abs(cmd->angular.z);
+      cmd->linear.x *= ratio;
+      cmd->angular.z *= ratio;
+    }
+    if (cmd->linear.x > 0 && std::abs(cmd->linear.x) < min_lin_vel_)
+    {
+      const double ratio = min_lin_vel_ / std::abs(cmd->linear.x);
+      cmd->linear.x *= ratio;
+      cmd->angular.z *= ratio;
+    }
+    else if (cmd->linear.x == 0 && std::abs(cmd->angular.z) < min_ang_vel_)
+    {
+      const double ratio = min_ang_vel_ / std::abs(cmd->angular.z);
+      cmd->linear.x *= ratio;
+      cmd->angular.z *= ratio;
+    }
   }
 
   rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_sub_;
@@ -130,11 +218,21 @@ private:
   rclcpp::callback_group::CallbackGroup::SharedPtr cb_grp3_;
 
   const double max_lin_vel_ = 0.3;
+  const double max_ang_vel_ = 0.5;
+  const double min_lin_vel_ = 0.05;
+  const double min_ang_vel_ = 0.1;
+  const double angle_tol_ = 5 * M_PI / 180.; // 5 degrees
+  const double lin_tol_ = 0.025; // 2.5 cm
+  
   const double TWO_M_PI_ = 2 * M_PI;
-  const double angle_tol_ = 5 * M_PI / 180.;
+
+  const double kP = 3;
+  const double kA = 8;
+  const double kB = -1.5;
 
   bool initial_goal_ = false;
   bool initial_gt_ = false;
+  bool print_once_ = false;
 };
 
 int main(int argc, char * argv[])
@@ -150,7 +248,6 @@ int main(int argc, char * argv[])
   executor.add_node(node);
 
   // Run node until it's exited
-  // rclcpp::spin(node);
   executor.spin();
 
   // Clean up
